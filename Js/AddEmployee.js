6e1f4ac7f901bd1f9398c3ab2c73900d6b1827b5
Milind_77 <milinddhote77@gmail.com>
2021-08-25 10:12:30 +0530

    [Milind] Refactor: Ability to do automatic update on gender.

--- Js/AddEmployee.js
+++ Js/AddEmployee.js
@@ -7,19 +7,70 @@ salaryValue.addEventListener("input", function () {
 });
 
 //Name validations
-var errorText = document.querySelector("#errorText");
+var errorName = document.querySelector("#errorname");
 var submitButton = document.querySelector("#submitButton");
 var fullName = document.querySelector("#fullName");
 var nameRegex = RegExp("^[A-Z]{1}[a-zA-Z]{3,}$");
 fullName.addEventListener("input", function () {
   if (nameRegex.test(fullName.value)) {
-    errorText.textContent = "";
+    errorName.textContent = "";
     submitButton.disabled = false;
-   } 
-   if (!nameRegex.test(fullName.value)) {
-    errorText.textContent =
+  }
+  if (!nameRegex.test(fullName.value)) {
+    errorName.textContent =
       "**Invalid!!, Name must Start with Uppercase and have min 3 characters";
     submitButton.disabled = true;
   }
-  
 });
+
+//Date Validations
+var day = document.querySelector("#day");
+var month = document.querySelector("#month");
+var year = document.querySelector("#year");
+var currentDate = new Date();
+
+function setCurrentDates() {
+  month.value = currentDate.getMonth();
+  day.value = currentDate.getDate();
+  year.value = currentDate.getFullYear();
+}
+
+function validateDate() {
+  //validation for future dates
+  if (
+    year.value == currentDate.getFullYear() &&
+    month.value <= currentDate.getMonth() &&
+    day.value <= currentDate.getDate()
+  ) {
+    errorText.textContent = "";
+  } else if (year.value < currentDate.getFullYear()) {
+    errorText.textContent = "";
+  } else if (year.value > currentDate.getFullYear()) {
+    setCurrentDates();
+  } else if (
+    year.value == currentDate.getFullYear() &&
+    month.value > currentDate.getMonth()
+  ) {
+    setCurrentDates();
+  } else {
+    setCurrentDates();
+  }
+}
+
+year.addEventListener("change", validateDate);
+month.addEventListener("change", validateDate);
+day.addEventListener("change", validateDate);
+
+
+//profile and gender validations
+var profileImage = document.getElementsByName('profile');
+var gender = document.getElementsByName('gender');
+for(let index =0; index < profileImage.length; index++){
+  profileImage[index].addEventListener("click", function(){
+    if(profileImage[0].checked || profileImage[2].checked){
+      gender[0].checked = true;
+    }else{
+      gender[1].checked = true;
+    }
+  });
+}
